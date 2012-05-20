99212cc1e2c685dc5fca9ef1453d930b2a4056bd
Aaron Williams <vesuvius@Aarons-MacBook-Air.local>
2012-05-20 15:59:40 -0700

    Initial commit

---
+++ summary.rb
@@ -0,0 +1,45 @@
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+# Grabbing the Cal Access Page
+page =  Nokogiri::HTML(open("http://cal-access.sos.ca.gov/Campaign/Committees/Detail.aspx?id=1342974&session=2011&view=general"))
+
+# Storing info in variables
+title = page.css('title').text
+
+# Basic info
+campaign_name =  page.css('#lblFilerName').text
+
+filer_id =  page.css('#_ctl3_lblFilerId').text
+
+filer_phone =  page.css('#_ctl3_lblFilerAddress').text
+
+# Nitty gritty
+
+current_status = page.css('#_ctl3_lblFilerAddressTable + table tr:nth-child(2) td:nth-child(2)').text
+last_report_date = page.css('#_ctl3_lblFilerAddressTable + table tr:nth-child(3) td:nth-child(2)').text
+reporting_period = page.css('#_ctl3_lblFilerAddressTable + table tr:nth-child(4) td:nth-child(2)').text
+contributions_this_period = page.css('#_ctl3_lblFilerAddressTable + table tr:nth-child(5) td:nth-child(2)').text
+total_contributions_this_period = page.css('#_ctl3_lblFilerAddressTable + table tr:nth-child(6) td:nth-child(2)').text
+expenditures_this_period = page.css('#_ctl3_lblFilerAddressTable + table tr:nth-child(7) td:nth-child(2)').text
+total_expenditures_this_period = page.css('#_ctl3_lblFilerAddressTable + table tr:nth-child(8) td:nth-child(2)').text
+ending_cash = page.css('#_ctl3_lblFilerAddressTable + table tr:nth-child(9) td:nth-child(2)').text
+
+
+# Print information to terminal
+puts # Spacing is pretty
+puts title + "\n" + campaign_name + "\n" + "Filer ID: " + filer_id + "\n" + "Filer Phone: " + filer_phone
+puts
+puts "Summary for " + campaign_name
+puts
+puts "Current Status: " + current_status
+puts "Last Report Date This Session: " + last_report_date
+puts "Reporting Period: " + reporting_period
+puts "Contributions from this period: " + contributions_this_period
+puts "Total Contributions " + reporting_period + ": " + total_contributions_this_period
+puts "Expenditures from this Period: " + expenditures_this_period
+puts "Total Expenditures " + reporting_period + ": " + total_expenditures_this_period
+puts "Ending Cash: " + ending_cash
+puts
