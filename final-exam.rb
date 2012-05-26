32e548b2710ca76e55b05891ad8185b343867796
aboutaaron <aaron.colby.williams@gmail.com>
2012-05-26 10:38:54 -0700

    Classes, classes, classes. Hashes, hashes, hashes.

--- final-exam.rb
+++ final-exam.rb
@@ -1,59 +1,49 @@
 require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
+require 'date'
 
 # Homepage of Elected Officials and Candidates running for California State Senate 2012
 campaign_data =  Nokogiri::HTML(open('http://cal-access.sos.ca.gov/Campaign/Candidates/'))
 
-# Grab Each Candidate link the
-campaign_data.css('a.sublink2').each do |candidates|
-	# Base URL
-	cal_access_url = "http://cal-access.sos.ca.gov"
+# Creating Candidate Class
+class Candidate
+	def initialize(url)
+		@url = url
+	end
 
-	#Grab Each Link
+	def get
+		candidate_page = Nokogiri::HTML(open(@url))
+
+		{
+			:political_party => candidate_page.css('span.hdr15').text,
+			:current_status => candidate_page.css('td tr:nth-child(2) td:nth-child(2) .txt7:first').text,
+			:last_report_date => candidate_page.css('td tr:nth-child(3) td:nth-child(2) .txt7:first').text,
+			:reporting_period => candidate_page.css('td tr:nth-child(4) td:nth-child(2) .txt7:first').text,
+			:contributions_this_period => candidate_page.css('td tr:nth-child(5) td:nth-child(2) .txt7:first').text.gsub(/[$,](?=\d)/, ''),
+			:total_contributions_this_period => candidate_page.css('td tr:nth-child(6) td:nth-child(2) .txt7:first').text.gsub(/[$,](?=\d)/, ''),
+			:expenditures_this_period => candidate_page.css('td tr:nth-child(7) td:nth-child(2) .txt7:first').text.gsub(/[$,](?=\d)/, ''),
+			:total_expenditures_this_period => candidate_page.css('td tr:nth-child(8) td:nth-child(2) .txt7:first').text.gsub(/[$,](?=\d)/, ''),
+			:ending_cash => candidate_page.css('td tr:nth-child(9) td:nth-child(2) .txt7:first').text.gsub(/[$,](?=\d)/, '')
+		}
+	end
+end
+
+
+# Begin scraper
+campaign_data.css('a.sublink2').each do |candidates|
+	
+	# Setting some variables
 	candidate_name =  candidates.text
+	cal_access_url = "http://cal-access.sos.ca.gov"
 	link_to_candidate = cal_access_url + candidates["href"]
 
 	# Confirming we grabbed the link in case of a slow-ass connection (Hello, Starbucks).
 	puts "Just grabbed the page for #{candidate_name}"
 	puts "the URL is #{link_to_candidate}"
 
-	# Opening the link
-	candidate_page = Nokogiri::HTML(open("#{link_to_candidate}"))
-
-	#####################
-	# Candidate Main Page
-	#####################
-
-	political_party = candidate_page.css('span.hdr15').text
-	current_status = candidate_page.css('td tr:nth-child(2) td:nth-child(2) .txt7').text
-	last_report_date = candidate_page.css('td tr:nth-child(3) td:nth-child(2) .txt7').text
-	reporting_period = candidate_page.css('td tr:nth-child(4) td:nth-child(2) .txt7').text
-	contributions_this_period = candidate_page.css('td tr:nth-child(5) td:nth-child(2) .txt7').text
-	total_contributions_this_period = candidate_page.css('td tr:nth-child(6) td:nth-child(2) .txt7').text
-	expenditures_this_period = candidate_page.css('td tr:nth-child(7) td:nth-child(2) .txt7').text
-	total_expenditures_this_period = candidate_page.css('td tr:nth-child(8) td:nth-child(2) .txt7').text
-	ending_cash = candidate_page.css('td tr:nth-child(8) td:nth-child(2) .txt7').text
-	
-	#####################
-	# Hash Browns
-	#####################
-
-
-	#####################
-	# Run
-	#####################
-	puts "Info for #{candidate_name}."
-	puts
-	puts "Political Party: #{political_party}"
-	puts "Current Status: #{current_status}"
-	puts "Last Report Date This Session: #{last_report_date}"
-	puts "Reporting Period: #{reporting_period}"
-	puts "Contributions from this period: #{contributions_this_period}"
-	puts "Total Contributions #{reporting_period}: #{total_contributions_this_period}"
-	puts "Expenditures from this Period: #{expenditures_this_period}"
-	puts "Total Expenditures #{reporting_period}: #{total_expenditures_this_period}"
-	puts "Ending Cash: #{ending_cash}"
+	# Initialize Candidate class and print Hash
+	p Candidate.new("http://cal-access.sos.ca.gov" + candidates["href"]).get
 	puts
 end
 
